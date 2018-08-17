9495c8fd621a63cd2283d4b54076e138e451a9ac
Zahi28 <xu_zixu@outlook.com>
2018-08-17 21:38:16 +0800

    添加了从图像中识别人脸的部分及一张数据图片

--- facemotion.py
+++ facemotion.py
@@ -1,3 +1,7 @@
 import face_recognition
 
 
+image = face_recognition.api.load_image_file("source_photo/01.JPG")
+face_locations = face_recognition.api.face_locations(image, number_of_times_to_upsample=1, model="hog")
+
+print(face_locations)
